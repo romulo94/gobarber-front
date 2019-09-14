2724ec91307252bc762a2b1fa742872766fd5d51
Romulo Rocha <romulorocha063@gmail.com>
2019-09-14 18:31:31 -0300

    adding persist in redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+import persistReducers from './persistReducers';
+
 import createStore from './createStore';
 
 import rootReducer from './modules/rootReducer';
@@ -13,8 +16,10 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middleware = [sagaMiddleware];
 
-const store = createStore(rootReducer, middleware);
+const store = createStore(persistReducers(rootReducer), middleware);
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
