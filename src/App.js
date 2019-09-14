1d022bf96640ae740579fcb43e7afa22e4f1dd09
Romulo Rocha <romulorocha063@gmail.com>
2019-09-14 17:30:02 -0300

    config redux/saga/store/reactotron

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import history from './services/history';
 import Routes from './routes';
+import store from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
