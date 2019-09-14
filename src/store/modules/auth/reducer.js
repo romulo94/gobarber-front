6a8128c4d8b87171bc23a52880f53e1ebb67ca2d
Romulo Rocha <romulorocha063@gmail.com>
2019-09-14 18:45:35 -0300

    adding loading in signin

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -9,14 +9,26 @@ const INITIAL_STATE = {
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case Types.SIGN_IN_SUCCESS:
-      return produce(state, draft => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case Types.SIGN_IN_REQUEST: {
+        draft.loading = true;
+        break;
+      }
+
+      case Types.SIGN_IN_SUCCESS: {
         draft.token = action.payload.token;
         draft.signed = true;
-      });
+        draft.loading = false;
+        break;
+      }
 
-    default:
-      return state;
-  }
+      case Types.SIGN_FAILURE: {
+        draft.loading = false;
+        break;
+      }
+
+      default:
+    }
+  });
 }
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,23 +1,27 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects';
-import { Types, signInSuccess } from './actions';
+import { Types, signInSuccess, signFailure } from './actions';
 import api from '~/services/api';
 import history from '~/services/history';
 
 export function* singIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, '/sessions', { email, password });
+    const response = yield call(api.post, '/sessions', { email, password });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário nao é prestador');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuário nao é prestador');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest(Types.SIGN_IN_REQUEST, singIn)]);
