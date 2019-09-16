068c57a582d9925f1ddf40b4c1c798bdf3d9afab
Romulo Rocha <romulorocha063@gmail.com>
2019-09-16 09:43:56 -0300

    request notification

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -35,6 +35,7 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 0px;
+  display: ${props => (props.visible ? 'block' : 'none')};
 
   &::before {
     content: '';
@@ -64,6 +65,7 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
   }
@@ -74,8 +76,7 @@ export const Notification = styled.div`
     background: none;
     color: ${lighten(0.2, '#7159c1')};
     padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
+    margin-bottom: 5px;
 
     ${props =>
       props.unread &&
@@ -87,6 +88,7 @@ export const Notification = styled.div`
           height: 8px;
           background: #ff892e;
           border-radius: 50%;
+          margin-left: 10px;
         }
       `}
   }
