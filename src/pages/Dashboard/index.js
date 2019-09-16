175b36164008be14dc37657dc0861aba4ea4ef21
Romulo Rocha <romulorocha063@gmail.com>
2019-09-16 20:58:11 -0300

    setting days

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import api from '~/services/api';
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   api.get('/appointments');
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button onClick={handlePrevDay} type="button">
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>31 de Maio</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button onClick={handleNextDay} type="button">
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
